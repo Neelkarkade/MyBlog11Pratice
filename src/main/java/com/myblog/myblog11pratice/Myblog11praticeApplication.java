e0aaf7d79b22419c7acaa90b9f28eb467299dbb5
Neelkarkade <neelkarkad0@gmail.com>
2024-08-06 17:04:54 +0530

    4444: Create save Post Features

---
+++ src/main/java/com/myblog/myblog11pratice/Myblog11praticeApplication.java
@@ -0,0 +1,13 @@
+package com.myblog.myblog11pratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myblog11praticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myblog11praticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myblog/myblog11pratice/Myblog11praticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myblog.myblog11pratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myblog11praticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
